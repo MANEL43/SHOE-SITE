7381758b24fcc2c5584af5aeccaf7f1b76d750c3
manuel <ngugimanu@gmail.com>
2024-06-17 14:52:06 +0300

    1.2.0

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {NavbarContainer} from '..styles.Navbar.style';
+function Navbar() {
+  return (
+    <div>
+      Navbar
+      <NavbarContainer></NavbarContainer>
+    </div>
+  );
+}
+
+export default App;
